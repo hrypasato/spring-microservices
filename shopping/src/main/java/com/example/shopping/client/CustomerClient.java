010c627ba3be0bb714a318059bf7a52731482f12
Henry Quinde <hrypasato@gmail.com>
2022-06-21 18:40:59 -0500

    feat[shopping]: add circuit braker on shopping service

--- shopping/src/main/java/com/example/shopping/client/CustomerClient.java
+++ shopping/src/main/java/com/example/shopping/client/CustomerClient.java
@@ -7,9 +7,20 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.example.shopping.model.Customer;
 
-@FeignClient(name = "customer-service", fallback = CustomerHystrixFallbackFactory.class)
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
+@FeignClient(name = "customer-service")
 public interface CustomerClient {
 
     @GetMapping(value = "/customers/{id}")
+    @CircuitBreaker(name = "customerCB",fallbackMethod = "getCustomerFallback")
     public ResponseEntity<Customer> getCustomer(@PathVariable("id") long id);
+    default ResponseEntity<Customer> getCustomerFallback(RuntimeException e){
+        Customer customer = Customer.builder()
+        .firstName("none")
+        .lastName("none")
+        .email("none")
+        .photoUrl("none").build();
+    return ResponseEntity.ok(customer);
+    }
 }
--- shopping/src/main/java/com/example/shopping/client/ProductClient.java
+++ shopping/src/main/java/com/example/shopping/client/ProductClient.java
@@ -8,14 +8,29 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.shopping.model.Product;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 @FeignClient(name = "product-service")
 public interface ProductClient {
 
-    @GetMapping(value = "/{id}")
+    @GetMapping(value = "products/{id}")
+    @CircuitBreaker(name = "productCB",fallbackMethod = "getProductFallback")
     public ResponseEntity<Product> getProduct(@PathVariable("id") Long id);
     
 
-    @GetMapping(value = "/{id}/stock")
+    @GetMapping(value = "products/{id}/stock")
+    @CircuitBreaker(name = "productCB",fallbackMethod = "getProductFallback")
     public ResponseEntity<Product> updateStockProduct(@PathVariable("id") Long id ,@RequestParam(name = "quantity", required = true) Double quantity);
+
+    default ResponseEntity<Product> getProductFallback(RuntimeException e ){
+        // Product product=Product.builder()
+        //         .id(0L)
+        //         .name("none")
+        //         .description("none")
+        //         .price(0.0)
+        //         .stock(0.0)
+        //         .build();
+        return ResponseEntity.ok(null);
+    }
 }
 
